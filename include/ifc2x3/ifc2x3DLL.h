f6e815488627a5d9ccd17b679a420623c33fb955
Mathieu MARACHE <mathieu.marache@cstb.fr>
2009-10-23 14:03:05 +0000

    added old header

--- include/ifc2x3/ifc2x3DLL.h
+++ include/ifc2x3/ifc2x3DLL.h
@@ -1,3 +1,29 @@
+/*
+//////////////////////////////////
+// This File has been generated //
+// by Expressik light generator //
+//  Powered by : Eve CSTB       //
+//////////////////////////////////
+
+ * *************************************************************************
+ *                                                                         *
+ *     STEP Early Classes C++                                              *
+ *                                                                         *
+ *     Copyright (C) 2009 CSTB                                             *
+ *                                                                         *
+ *                                                                         *
+ *   For further information please contact                                *
+ *                                                                         *
+ *         eve@cstb.fr                                                     *
+ *   or                                                                    *
+ *         Mod-Eve, CSTB                                                   *
+ *         290, route des Lucioles                                         *
+ *         BP 209                                                          *
+ *         06904 Sophia Antipolis, France                                  *
+ *                                                                         *
+ ***************************************************************************
+*/
+
 #ifdef _MSC_VER
 #	pragma warning(disable : 4251) // [...] needs to have dll-interface								//  to be used by clients of [...]
 #	ifdef _STATIC_IFC2X3
