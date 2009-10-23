64411938a712e73c75db16c1edb99f70815ce7b0
Mathieu MARACHE <mathieu.marache@cstb.fr>
2009-10-23 14:06:31 +0000

    added new cartouche

--- include/ifc2x3/IfcPhysicalComplexQuantity.h
+++ include/ifc2x3/IfcPhysicalComplexQuantity.h
@@ -1,3 +1,19 @@
+// IFC SDK : IFC2X3 C++ Early Classes  
+// Copyright (C) 2009 CSTB
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full license is in Licence.txt file included with this 
+// distribution or is available at :
+//     http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
 #ifndef IFC2X3_IFCPHYSICALCOMPLEXQUANTITY_H
 #define IFC2X3_IFCPHYSICALCOMPLEXQUANTITY_H
 #include <ifc2x3/DefinedTypes.h>
