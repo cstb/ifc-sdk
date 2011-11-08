81f53ec23ee8bb94a1ac7c00bd86243d85e147c7
Mathieu MARACHE <mathieu.marache@cstb.fr>
2011-11-08 12:30:59 +0100

    works on mac

--- src/ifc2x3/IfcRightCircularCone.cpp
+++ src/ifc2x3/IfcRightCircularCone.cpp
@@ -14,7 +14,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
 
-#ifdef ENABLE_MSVC_PRECOMPILED_HEADER
+#ifdef ENABLE_PRECOMPILED_HEADER
 #include "precompiled.h"
 #endif
 
