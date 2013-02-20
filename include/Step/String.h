0d53a396ba4b28ce63f5759dafe0e8d4d104e0d0
Kike <kike@asuni.com>
2013-02-20 20:45:30 +0100

    1. Removed Step::String::alphabet is not necessary at all. Step::String always contains UTF16 strings. An ISO8859 alphabet is computed when needed. The standard allows strings encoded with multiple alphabets, this implementation take advantage of that.
    2. Also fixed a bug that makes the function parse string loop infinitely when the string contains incomplete escape sequences.
    
    3. Now we only use special characters (\s\) from ISO-8859 if the character is defined. The conversion to not defined characters returns 0x0000 and is encoded as extended characters (\X\, \X2\, \X4\) as necessary.

--- include/Step/String.h
+++ include/Step/String.h
@@ -1,11 +1,11 @@
-// IFC SDK : IFC2X3 C++ Early Classes
+// IFC SDK : IFC2X3 C++ Early Classes  
 // Copyright (C) 2009 CSTB
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
-// The full license is in Licence.txt file included with this
+// The full license is in Licence.txt file included with this 
 // distribution or is available at :
 //     http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 //
@@ -32,7 +32,7 @@ namespace Step {
     {
     public:
         /**
-         * Default constructor, set Alphabet used to unknown
+         * Default constructor
          */
         String();
 
@@ -43,75 +43,54 @@ namespace Step {
         String(const String& str);
 
         /**
-         * Copy constructor, set Alphabet used to unknown
+         * Copy constructor, expects UTF16 encoding
          * \param str the wchar_t array to copy
          */
         String(const wchar_t *str);
 
         /**
-         * Copy constructor, set Alphabet used to unknown
+         * Copy constructor, expects UTF16 encoding
          * \param str the std::wstring to copy
          */
         String(const std::wstring& str);
 
         /**
-         * Copy constructor, set Alphabet used to Western_European
-         * expects ISO 8859-(default alphabet code) encoding
+         * Copy constructor, expects UTF8 encoding
          *  \param str the char array to copy
          */
         String(const char *str);
         /**
-         * Copy constructor, set Alphabet used to Western_European
-         * expects ISO 8859-(default alphabet code) encoding
+         * Copy constructor, expects UTF8 encoding
          * \param str the std::string to copy
          */
         String(const std::string &str);
 
-        /**
-         * Virtual destructor
-         */
-        virtual ~String();
+		/**
+		 * Virtual destructor
+		 */
+		virtual ~String();
 
         /**
-         * compares to a String to a ISO 8859-(default alphabet code) encoded representation
+         * compares to a UTF8 encoded representation
          * \param str the string to compare too
          * \return success
          */
         virtual bool operator==(const std::string &str) const;
 
         /**
-         * returns a ISO 8859-(alphabet code) encoded representation
-         * \return a ISO 8859-(alphabet code) std::string
-         */
-        virtual std::string toISO_8859() const;
-
-        /**
-         * returns a UTF8 encoded representation
-         * \return a UTF8 std::string
+         * returns a UTF8 (multi character) encoded representation
+         * \return a UTF8 (multi character) std::string
          */
         virtual std::string toUTF8() const;
 
         /**
-         * builds from a UTF8 encoded representation
+         * builds from a UTF8 (multi character) encoded representation
          * \param str the string to convert
          * \return the String created
          */
         static String fromUTF8(const std::string &str);
 
-        /**
-         * returns a ISO 10303-21 SPF encoded string representation
-         * \return a ISO 10303-21 SPF std::string
-         */
-        virtual std::string toSPF() const;
-
-        /**
-         * builds from a ISO 10303-21 SPF  encoded representation
-         * \param str the string to convert
-         * \return the String created
-         */
-        static String fromSPF(const std::string &str);
-
-        /**
+		/**
          * The list of Alphabet supported
          */
         typedef enum
@@ -127,28 +106,34 @@ namespace Step {
             Hebrew,
             Turkish
         } Alphabet;
+		
+        /**
+         * returns a (ISO 8859-X) encoded representation
+         * \param alphabet the alphabet used to encode returned string
+         * \return a (ISO 8859-X) std::string
+         */
+        virtual std::string toISO_8859(Alphabet alphabet) const;
 
         /**
-         * Set the Alphabet. Used in SPF encodings
-         * \param a the Alphabet
+         * builds from a (ISO 8859-X) encoded representation
+         * \param str the string to convert
+         * \param alphabet the alphabet to convert str from
+         * \return the String created
          */
-        virtual void setAlphabet(Alphabet a);
+        static String fromISO_8859(const std::string &str, Alphabet alphabet);
 
         /**
-         * get Alphabet used by this String
-         * \return the Alphabet of this String
+         * returns a ISO 10303-21 SPF encoded string representation
+         * \return a ISO 10303-21 SPF std::string
          */
-        virtual Alphabet getAlphabet() const;
+        virtual std::string toSPF() const;
 
         /**
-         * set default Alphabet for all subsequent new String creation
+         * builds from a ISO 10303-21 SPF  encoded representation
+         * \param str the string to convert
+         * \return the String created
          */
-        static void setDefaultAlphabet(Alphabet a);
-
-
-    private:
-        Alphabet alphabet;
-        void buildISO_8859(const std::string &str);
+        static String fromSPF(const std::string &str);
     };
 
 }
@@ -159,5 +144,12 @@ namespace Step {
  **  \sa toUTF8()
  */
 STEP_EXPORT std::ostream& operator<<(std::ostream &out, const Step::String &s);
+
+/*!
+ **  inputs an UTF8 (multi character) from an encoded std::string representation
+ **  is equivalent to calling std:string str; in >> str; String s = String::fromUTF8(str);
+ **  \sa toUTF8()
+ */
+STEP_EXPORT std::istream& operator>>(std::istream &in, Step::String &s);
 
 #endif
